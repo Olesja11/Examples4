41135989fb16443ae47633baeb849e58bd819dc6
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-09-04 22:47:07 +0300

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход
+// число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("Введи число");
+int A = int.Parse(Console.ReadLine());
+int sum_of_digits = 0;
+int digit;
+while(A > 0) 
+{
+    digit = A %10;
+    sum_of_digits  = sum_of_digits + digit;
+    A = A /10;
+}
+Console.WriteLine($"Сумма цифр = {sum_of_digits}");
