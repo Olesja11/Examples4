54727090e186daf3284495096979fb04c87a3ebc
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-09-04 22:46:49 +0300

    Add Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+Console.Clear();
+Console.WriteLine("Введи А");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи B");
+int B = int.Parse(Console.ReadLine());
+int stepen = 1;
+for(int i = 0; i < B;i++) 
+{
+    stepen = stepen * A;
+}
+Console.WriteLine($"Степень А = {stepen}");
