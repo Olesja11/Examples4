aa501f399080fbe38403563609b8fa7fc6379320
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-09-05 10:20:45 +0300

    Add Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+Console.Clear();
+Console.WriteLine("Введи количество элементов массива");
+int count = int.Parse(Console.ReadLine());
+int [] array = new int [count];
+for (int i = 0; i < count; i++ ) 
+{
+    array [i] = int.Parse(Console.ReadLine());
+    
+}
+for (int i = 0; i < count; i++ ) 
+{
+    Console.Write ($"{array[i]} ");
+}
