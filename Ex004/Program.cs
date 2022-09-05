5fea12a3fb05a95b696f2e75b80b77862f6f522e
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-09-05 10:34:20 +0300

    Add Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 10 элементов, 
+// которые необходимо заполнить случайными значениями в диапазоне
+//  от -10 до 10 и найти максимальное значение среди них
+Console.Clear();
+int [] array = new int [10];
+for (int i = 0; i < array.Length; i++ ) 
+{
+    array [i] = new Random().Next(-10,11);
+    Console.Write ($"{array[i]} ");    
+}
+int max = array [0];
+for (int i = 0; i < array.Length; i++ ) 
+{
+   if (array[i]> max) 
+   {
+        max = array [i];
+    }
+}
+Console.WriteLine ($"max = {max} ");
